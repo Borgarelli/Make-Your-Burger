547ea2e695fe2e4c04f58082d8ff3e8a81684a23
Borgarelli <borgarellitavares@outlook.com>
2022-11-12 23:30:39 -0300

    bug fix

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <Navbar />
+    <Navbar /> <!--Importa o componete de Navbar que contém os links definidos com o VueRouter-->
+    <router-view/> <!--A view é feita através desse componente aqui, ou seja os outros componentes podem ser repetidos por estarem fora dessaa tag -->
+    <Footer />
   </div>
-   <router-view/> <!--A view é feita através desse componente aqui, ou seja os outros componentes podem ser repetidos por estarem fora dessaa tag -->
-  <Footer />
 </template>
 
 <script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
     <div id="nav" >
-        <RouterLink to="/">
+        <router-link to="/">
             <img src="" id="logo">
-        </RouterLink>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/pedidos">Pedidos</router-link>
+        </router-link>
+        <router-link to="/">Home</router-link> | <!--Estabelece o link com o componente home atráves desse "to="/"" que referencia ao path de home, isso é definido dentro da pasta Router-->
+        <router-link to="/pedidos">Pedidos</router-link> <!--Estabelece o link com o componente Pedidos atráves do "to=" que referencia ao path de Pedidos, definido dentro da pasta Router-->
     </div>
 </template>
 
